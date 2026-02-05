7ce70cc7184a10a1d31fd308495fc37d900be93d
Naman Gera <namangera15@gmail.com>
2026-02-05 14:52:21 +0000

    Add multi file data loading module

---
+++ ml4deltabot/__init__.py
@@ -0,0 +1,9 @@
+"""
+Deltabot Neural Network Model
+
+A package for fast neural network inference on FPGA for real-time control
+of a non-linear deltabot system at Diamond Light Source.
+"""
+
+__version__ = "0.1.0"
+
